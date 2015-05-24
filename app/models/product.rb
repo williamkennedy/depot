1413434281d773ceb32dfb9952313bf5b72bbc2c
williamkennedy <williamkennedyjnr@gmail.com>
2015-05-24 13:08:23 +0100

    customer interaction, price format

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true, length: { minimum: 10, too_short: "must have at least 10 letters" }
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,6 +16,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_description'
+    assert_select '.list_actions'
   end
 
   test "should get new" do
