b648e8ac539158e7297e63e06a74ab7d845af66c
williamkennedy <williamkennedyjnr@gmail.com>
2015-06-21 23:16:14 +0100

    Finished iteration H1, adding mailer actions

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,6 @@ class Order < ActiveRecord::Base
 	has_many :payments
 	validates :name, :address, :email, presence: true
 
-	def assign_pay_type
-		self.pay_type = Payment.type
-	end
 
 	def add_line_items_from_cart(cart)
 		cart.line_items.each do |item|
---
+++ test/integration/user_stories_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UserStoriesTest < ActionDispatch::IntegrationTest
+ fixtures :products
+
+ LineItem.delete_all
+ Order.delete_all
+ ruby_book = products(:ruby)
+end
