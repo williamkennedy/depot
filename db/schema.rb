e4d23d0976d38a7f4966d150bc723c5b84d2cf8a
williamkennedy <williamkennedyjnr@gmail.com>
2015-07-07 22:56:37 +0100

    started adding user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615190111) do
+ActiveRecord::Schema.define(version: 20150707211321) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150615190111) do
     t.string   "pay_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ship_date"
   end
 
   create_table "payments", force: true do |t|
@@ -52,6 +53,13 @@ ActiveRecord::Schema.define(version: 20150615190111) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
